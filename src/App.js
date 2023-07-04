0ac907626e3910382848354997339fc686d15d32
yup22cxu <yup22cxu@uea.ac.uk>
2023-07-04 16:50:41 +0100

    endoftut

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API } from "aws-amplify";
 import { API, Storage } from 'aws-amplify';
 import {
   Button,
