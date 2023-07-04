c551d42311119d69e5953735d17cc3b12245ca9a
yup22cxu <yup22cxu@uea.ac.uk>
2023-07-04 10:23:44 +0100

    changes for v2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>
-        <p> philly is a little stinky</p>
       </header>
     </div>
   );
